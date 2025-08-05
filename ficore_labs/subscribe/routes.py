680cfbe09d56234c01a73583b78621be860282d1
abumeemah <abumeemah@gmail.com>
2025-08-05 10:47:03 +0100

    Update routes.py

--- ficore_labs/subscribe/routes.py
+++ ficore_labs/subscribe/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, current_app
+from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify, current_app, session
 from flask_login import login_required, current_user
 from flask_wtf.csrf import CSRFError
 from translations import trans
